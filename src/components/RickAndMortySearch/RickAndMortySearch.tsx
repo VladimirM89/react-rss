997898c0eaa3f164369dfe82a148e27ec8ebe5ff
Vladimir <v.milashevsky@gmail.com>
2023-10-27 00:28:58 +0300

    refactor:  minor refactoring; add input and submit handlers

--- src/components/RickAndMortySearch/RickAndMortySearch.tsx
+++ src/components/RickAndMortySearch/RickAndMortySearch.tsx
@@ -1,44 +1,17 @@
 import { Component } from 'react';
 import { SearchBar } from '../SearchBar/SearchBar';
 import { SearchList } from '../SearchList/SearchList';
-import searchApiAxios from '../../api/SearchApi';
-import { SearchResponseInterface, CharacterInterface } from '../../interfaces/SearchResponse';
-import getAllCharacterIds from '../../utils/queryParams';
-
-interface SearchParams {
-  name?: string;
-  page?: number;
-}
+import { CharacterInterface } from '../../interfaces/SearchResponse';
 
 type State = {
-  inputValue: '';
+  characters: Array<CharacterInterface>;
 };
 
 export class RickAndMortySearch extends Component {
   state: Readonly<State> = {
-    inputValue: '',
+    characters: [],
   };
 
-  async componentDidMount(): Promise<void> {
-    this.getCharacters();
-  }
-
-  async getCharacters(params: SearchParams | null = null) {
-    if (!params) {
-      const response = await searchApiAxios.get<SearchResponseInterface>('');
-      const charactersCount = response.data.info.count;
-
-      const stringOfIds = getAllCharacterIds(charactersCount);
-
-      const allChars = await searchApiAxios.get<CharacterInterface>(`${stringOfIds}`);
-      console.log('all chars:', allChars.data);
-    } else {
-      const response = await searchApiAxios.get<SearchResponseInterface>(``, {
-        params,
-      });
-      console.log('filtered chars', response.data);
-    }
-  }
   render() {
     return (
       <div>
--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,10 +1,70 @@
-import { Component } from 'react';
-export class SearchBar extends Component {
+import { Component, createRef } from 'react';
+import searchApiAxios from '../../api/SearchApi';
+import { SearchResponseInterface, CharacterInterface } from '../../interfaces/SearchResponse';
+import getAllCharacterIds from '../../utils/queryParams';
+
+interface SearchParams {
+  name?: string;
+  page?: number;
+}
+
+type State = {
+  inputValue: string;
+};
+export class SearchBar extends Component<object, State> {
+  state: Readonly<State> = {
+    inputValue: '',
+  };
+
+  inputRef: React.RefObject<HTMLInputElement> = createRef();
+
+  async componentDidMount(): Promise<void> {
+    this.inputRef.current?.focus();
+    // this.getCharacters();
+  }
+
+  async getCharacters(params: SearchParams | null = null): Promise<void> {
+    if (!!this.state.inputValue) {
+      const response = await searchApiAxios.get<SearchResponseInterface>(``, {
+        params,
+      });
+      console.log('filtered chars', response.data);
+    } else {
+      const response = await searchApiAxios.get<SearchResponseInterface>('');
+      const charactersCount = response.data.info.count;
+
+      const stringOfIds = getAllCharacterIds(charactersCount);
+
+      const allChars = await searchApiAxios.get<CharacterInterface>(`${stringOfIds}`);
+      console.log('all chars:', allChars.data);
+    }
+  }
+
+  private handleInput = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    this.setState({
+      inputValue: event.target.value,
+    });
+  };
+
+  private handleSearch = (event: React.FormEvent<HTMLFormElement | HTMLButtonElement>): void => {
+    this.getCharacters({
+      name: this.state.inputValue,
+    });
+
+    this.inputRef.current?.blur();
+    event.preventDefault();
+  };
+
   render() {
     return (
       <div>
-        <form>
-          <input />
+        <form onSubmit={this.handleSearch}>
+          <input
+            type="text"
+            ref={this.inputRef}
+            value={this.state.inputValue}
+            onChange={this.handleInput}
+          />
           <button type="submit">Search</button>
         </form>
       </div>
