dac27d7ade391befc19e7ce8cc57d39b6d2e5664
Vladimir <v.milashevsky@gmail.com>
2023-10-31 01:16:12 +0300

    feat: make Header and LoaderComponent component as a function components

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,11 @@
-import { Component } from 'react';
+import { FC } from 'react';
 import cn from 'classnames';
 import styles from './Header.module.scss';
 
-export class Header extends Component {
-  render() {
-    return (
-      <header className={cn('wrapper', styles.header_wrapper)}>
-        <h2 className={styles.header_title}>Rick and Morty Search App</h2>
-      </header>
-    );
-  }
-}
+export const Header: FC = () => {
+  return (
+    <header className={cn('wrapper', styles.header_wrapper)}>
+      <h2 className={styles.header_title}>Rick and Morty Search App</h2>
+    </header>
+  );
+};
--- src/components/LoaderComponent/LoaderComponent.tsx
+++ src/components/LoaderComponent/LoaderComponent.tsx
@@ -1,12 +1,10 @@
-import { Component } from 'react';
+import { FC } from 'react';
 import styles from './LoaderComponent.module.scss';
 
-export class LoaderComponent extends Component {
-  render() {
-    return (
-      <div className={styles.loader_container}>
-        <div className={styles.loader}></div>
-      </div>
-    );
-  }
-}
+export const LoaderComponent: FC = () => {
+  return (
+    <div className={styles.loader_container}>
+      <div className={styles.loader}></div>
+    </div>
+  );
+};
