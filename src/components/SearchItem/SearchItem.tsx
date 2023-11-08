2b28100a0f52f676916ec0450a840d258df75eec
Vladimir <v.milashevsky@gmail.com>
2023-11-08 22:09:41 +0300

    refactor: refactor SearchList and SearchItem components

--- src/components/SearchItem/SearchItem.tsx
+++ src/components/SearchItem/SearchItem.tsx
@@ -1,15 +1,40 @@
 import { FC } from 'react';
 import { CharacterInterface } from '../../interfaces/SearchResponseInterfaces';
 import styles from './SearchItem.module.scss';
+import { useNavigate, createSearchParams, useSearchParams } from 'react-router-dom';
+import { customCreateSearchParams } from '../../utils/queryParams';
 
 type SearchItemProps = {
   item: CharacterInterface;
 };
 
 export const SearchItem: FC<SearchItemProps> = ({ item }) => {
-  // const { title_english, images.jpg.small_image_url } = item;
+  const [searchParams] = useSearchParams();
+  const searchParam = searchParams.get('q');
+  const pageParam = searchParams.get('page');
+  const limitParam = searchParams.get('limit');
+
+  const navigate = useNavigate();
+
+  const handleNavigate = (id: number) => {
+    const checkedParams = customCreateSearchParams({
+      q: searchParam || '',
+      page: Number(pageParam),
+      limit: Number(limitParam),
+    });
+
+    navigate({
+      pathname: '/',
+      search: createSearchParams({ ...checkedParams, details: `${id}` }).toString(),
+    });
+  };
+
   return (
-    <li className={styles.card_container}>
+    <li
+      className={styles.card_container}
+      key={item.mal_id}
+      onClick={() => handleNavigate(item.mal_id)}
+    >
       <img
         className={styles.character_image}
         src={item.images.jpg.image_url}
--- src/components/SearchList/SearchList.tsx
+++ src/components/SearchList/SearchList.tsx
@@ -3,40 +3,14 @@ import { SearchItem } from '../SearchItem/SearchItem';
 import cn from 'classnames';
 import styles from './SearchList.module.scss';
 import { FC } from 'react';
-import { createSearchParams, useNavigate, useSearchParams } from 'react-router-dom';
-import { customCreateSearchParams } from '../../utils/queryParams';
 
 type SearchListProps = {
   list: Array<CharacterInterface>;
 };
 
 export const SearchList: FC<SearchListProps> = ({ list }) => {
-  const [searchParams] = useSearchParams();
-  const searchParam = searchParams.get('q');
-  const pageParam = searchParams.get('page');
-  const limitParam = searchParams.get('limit');
-
-  const navigate = useNavigate();
-
-  const handleNavigate = (id: number) => {
-    const checkedParams = customCreateSearchParams({
-      q: searchParam || '',
-      page: Number(pageParam),
-      limit: Number(limitParam),
-    });
-
-    navigate({
-      pathname: '/',
-      search: createSearchParams({ ...checkedParams, details: `${id}` }).toString(),
-    });
-  };
-
   const renderedList = list.map((item: CharacterInterface) => {
-    return (
-      <a key={item.mal_id} onClick={() => handleNavigate(item.mal_id)}>
-        <SearchItem item={item} />
-      </a>
-    );
+    return <SearchItem item={item} key={item.mal_id} />;
   });
   return <ul className={cn('wrapper', styles.list_container)}>{renderedList}</ul>;
 };
