ce84f79b758bd51504155ccf0e090d02caa5e3c1
Vladimir <v.milashevsky@gmail.com>
2023-11-19 23:34:36 +0300

    fix: fix vitest timeout before pre-push tests

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   test: {
-    // watch: false,
+    watch: false,
+    teardownTimeout: 20000,
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/test/setup.ts',
