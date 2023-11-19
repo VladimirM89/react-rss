e1ad08cc598368b9bf9167f0cb8448535802f03f
Vladimir <v.milashevsky@gmail.com>
2023-11-19 23:59:17 +0300

    fix: fix vitest timeout

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [react()],
   test: {
     watch: false,
-    teardownTimeout: 50000,
+    teardownTimeout: 100000,
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/test/setup.ts',
